5e7d70088303d382c7a9997edf85cc065da9717a
Conrado1212 <conrado1212@gmail.com>
2021-01-06 18:44:11 +0100

    Version 1.02

--- src/main/java/pl/edu/wszib/controller/usercontroller/LoginController.java
+++ src/main/java/pl/edu/wszib/controller/usercontroller/LoginController.java
@@ -1,12 +1,15 @@
 package pl.edu.wszib.controller.usercontroller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import pl.edu.wszib.SessionObject;
 import pl.edu.wszib.model.User;
+import pl.edu.wszib.service.IAuthenticationService;
 
 import javax.annotation.Resource;
 
@@ -15,12 +18,20 @@ public class LoginController {
     @Resource(name = "sessionObject")
     SessionObject sessionObject;
 
+    @Autowired
+    IAuthenticationService authenticationService;
+
+    public LoginController(){
+
+    }
+
+
     @RequestMapping(value = "",method = RequestMethod.GET)
     public String mainSite(){
         return "redirect:login";
     }
 
-    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    /*@RequestMapping(value = "/login", method = RequestMethod.GET)
     public ModelAndView loginPage( ) {
         return new ModelAndView("login", "loginKey", new User());
     }
@@ -35,6 +46,26 @@ public class LoginController {
         return "login";
     }
 
+     */
+
+    @RequestMapping(value = "/login",method = RequestMethod.GET)
+    public String showLoginForm(Model model){
+        model.addAttribute("userModel",new User());
+        model.addAttribute("errorMessage","");
+        return "login";
+    }
+
+    @RequestMapping(value = "/authenticate",method = RequestMethod.POST)
+    public  String authenticateUser(@ModelAttribute("userModel")User user,Model model){
+        boolean authResult = this.authenticationService.authenticationUser(user);
+        if(authResult){
+            return "rentAppPage";
+        }else{
+            model.addAttribute("errorMessage","zle dane!!!");
+            model.addAttribute("userModel",new User());
+            return "login";
+        }
+    }
 
     @RequestMapping(value = "/logout",method = RequestMethod.GET)
     public String logout(){
--- src/main/java/pl/edu/wszib/controller/usercontroller/RegisterController.java
+++ src/main/java/pl/edu/wszib/controller/usercontroller/RegisterController.java
@@ -2,12 +2,15 @@ package pl.edu.wszib.controller.usercontroller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import pl.edu.wszib.dao.impl.UserDAOImpl;
+import pl.edu.wszib.model.RegisterUser;
 import pl.edu.wszib.model.User;
+import pl.edu.wszib.service.IUserService;
 
 
 @Controller
@@ -16,6 +19,13 @@ public class RegisterController {
     @Autowired
     UserDAOImpl userDAO;
 
+    @Autowired
+    IUserService userService;
+
+    public RegisterController() {
+
+    }
+    /*
     @RequestMapping(value = "/register",method = RequestMethod.GET)
     public ModelAndView Registry(){
         return new ModelAndView("register","addUserKey",new User());
@@ -28,8 +38,41 @@ public class RegisterController {
         return  "login";
     }
 
+     */
 
 
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
+    public String registerForm(Model model) {
+        model.addAttribute("userModel", new RegisterUser());
+        model.addAttribute("incorrectRepeat", "");
+        return "register";
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public String register(@ModelAttribute RegisterUser registerUser, Model model) {
+        if (registerUser.getPassword().equals(registerUser.getRepeatPass())) {
+            this.userService.registerUser(this.convertRegisterUserToUser(registerUser));
+            return "redirect:login";
+        } else {
+            model.addAttribute("userModel", new RegisterUser());
+            model.addAttribute("incorrectRepeat", "Zle haslo");
+            return "register";
+        }
+    }
+
+    private User convertRegisterUserToUser(RegisterUser registerUser) {
+        User user = new User();
+        user.setUsername(registerUser.getUsername());
+        user.setPassword(registerUser.getPassword());
+        user.setName(registerUser.getName());
+        user.setSurname(registerUser.getSurname());
+        user.setEmail(registerUser.getEmail());
+        user.setLongitude(registerUser.getLongitude());
+        user.setLatitude(registerUser.getLatitude());
+        user.setRole(registerUser.getRole());
+        user.setLocationName(registerUser.getLocationName());
+        return user;
+    }
 
 
 }
